53bcebad252700357489e5aa157615c9eaf63b2b
James Miranda <james.miranda@travelexplorer.com.br>
2017-06-23 17:57:05 -0300

    First commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function show()
+    {
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $this->validate(request(), [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        return redirect('/');
+    }
+}
